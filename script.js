3466868f567806b2363687c2e74bc23eb3297c52
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-17 23:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+	const table = document.getElementById('sampleTable');
+    const newRow = table.insertRow(0);
+    const newCell1 = newRow.insertCell(0);
+    const newCell2 = newRow.insertCell(1);
+    newCell1.textContent = 'New Cell1';
+    newCell2.textContent = 'New Cell2';
   
 }
